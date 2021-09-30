f47ec4707f762ac75254af381c0921911ac048a1
Rob Beattie <beattier@mac.com>
2021-09-30 14:26:16 +0100

    dfsd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value} from an azure static webapp</div>;
+  baseUrl = '/api/test-function'
+
+  const [stateVariable, setStateVariable] = useState('');
+  useEffect(() => {
+    setStateVariable("Fred")
+    axios.get(baseUrl).then((response) => {
+      setStateVariable(response.data)
+    })
+    return () => {
+      
+    }
+  }, [])
+
+  return <div>Hello {stateVariable} from an azure static webapp</div>;
 }
 
 export default App;
